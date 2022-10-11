a8a886ed1c82885ce95882c97f7deb2203704bf8
kingchan223 <kokiyo97@naver.com>
2022-10-11 13:51:40 +0900

    add @Sql, book delete test complete

--- src/test/java/com/example/junitstudy/domain/BookRepositoryTest.java
+++ src/test/java/com/example/junitstudy/domain/BookRepositoryTest.java
@@ -1,10 +1,18 @@
 package com.example.junitstudy.domain;
 
+import org.aspectj.lang.annotation.Before;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.jdbc.Sql;
 
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @DataJpaTest /* --> DB와 관련된 컴포넌트만 메모리에 로딩 (즉 controller, service layer 는 메모리에 로딩 X) */
@@ -14,9 +22,22 @@ public class BookRepositoryTest {
     @Autowired
     private BookRepository bookRepository;
 
+    //@BeforeAll // 테스트 시작 전에 한번만 실행
+    @BeforeEach  // 모든 각 테스트 시작 전에 한번씩 실행
+    void 데이터_준비() {
+        String title = "junit";
+        String author = "chan";
+        Book book = Book.builder()
+                .title(title)
+                .author(author).build();
+
+        bookRepository.save(book);
+    }// 얘는 메서드가 끝난다고 트랜잭션 종료 안됨. --> rollback 안됨
+     // @BeforeEach 는 바로 다음에 실행되는 메서드까지만 트랜잭션이 묶이게 된다.
+
     // 1. 책 등록
     @Test
-    public void 책_등록() {
+    void 책_등록() {
         //given (데이터 준비)
         String title = "junit5";
         String author = "lee";
@@ -30,5 +51,48 @@ public class BookRepositoryTest {
         //then (검증)
         assertEquals(title, bookPS.getTitle());
         assertEquals(author, bookPS.getAuthor());
-    }
+    } // transaction close --> rollback(init saved data)
+
+    @Test
+    void 책_목록_조회() {
+        //given (데이터 준비)
+        String title = "junit";
+        String author = "chan";
+
+        //when (테스트 실행)
+        List<Book> booksPS = bookRepository.findAll();
+
+        //then (검증)
+        assertEquals(title, booksPS.get(0).getTitle());
+        assertEquals(author, booksPS.get(0).getAuthor());
+        assertEquals(1, booksPS.size());
+    } // transaction close --> rollback(init saved data)
+
+    @Sql("classpath:db/tableInit.sql")
+    @Test
+    void 책_한권_조회() {
+        //given (데이터 준비)
+        String title = "junit";
+        String author = "chan";
+
+        //when (테스트 실행)
+        Book bookPS = bookRepository.findById(1L).get();
+
+        //then (검증)
+        assertEquals(title, bookPS.getTitle());
+        assertEquals(author, bookPS.getAuthor());
+    } // transaction close --> rollback(init saved data)
+
+    @Sql("classpath:db/tableInit.sql")
+    @Test
+    void 책_한권_삭제() {
+        //given (데이터 준비)
+        Long id = 1L;
+
+        //when (테스트 실행)
+        bookRepository.deleteById(id);
+
+        //then (검증)
+        assertFalse(bookRepository.findById(id).isPresent());
+    } // transaction close --> rollback(init saved data)
 }
